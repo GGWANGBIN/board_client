f8f094abc60ef174643cc87db33efa10401a1c98
GGWANBIN <fprhffjtm30@naver.com>
2023-06-16 09:20:23 +0900

    깃허브 공유 커밋

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import axios from "axios";
+import Login from "./View/Login";
+import SignUp from "./View/SignUp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    const no = 1;
+
+    useEffect(() => {
+        axios.post('/hello',{no})
+            .then(response => setFf(response.data))
+    }, [])
+
+    const [ff,setFf] = useState([]);
+
+    return(
+        <Login>
+        </Login>
+    );
 }
 
 export default App;
